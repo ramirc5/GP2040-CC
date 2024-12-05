bd8b461e056222b25205fc879ece55a290318804
Cristian <125297328+ramirc5@users.noreply.github.com>
2024-12-04 22:20:19 -0800

    done?

--- configs/CrossCut/BoardConfig.h
+++ configs/CrossCut/BoardConfig.h
@@ -29,7 +29,7 @@
 #define GPIO_PIN_00 GpioAction::BUTTON_PRESS_S2    // S2     | Start  | Plus    | Start    | 10     | Start  |
 #define GPIO_PIN_14 GpioAction::BUTTON_PRESS_L3    // L3     | LS     | LS      | L3       | 11     | LS     |
 #define GPIO_PIN_12 GpioAction::BUTTON_PRESS_R3    // R3     | RS     | RS      | R3       | 12     | RS     |
-#define GPIO_PIN_12 GpioAction::BUTTON_PRESS_A2    // A2     | ~      | Capture | ~        | 14     | ~      |
+#define GPIO_PIN_15 GpioAction::BUTTON_PRESS_A2    // A2     | ~      | Capture | ~        | 14     | ~      |
 #define GPIO_PIN_07 GpioAction::BUTTON_PRESS_FN    // Hotkey Function                                        |
 
 // Setting GPIO pins to assigned by add-on
--- headers/buttonlayouts.h
+++ headers/buttonlayouts.h
@@ -288,9 +288,10 @@
             {GP_ELEMENT_BTN_BUTTON, {113, 14, 4, 4, 1, 1, GAMEPAD_MASK_R1, GP_SHAPE_ELLIPSE}}, \
             {GP_ELEMENT_BTN_BUTTON, {122, 19, 4, 4, 1, 1, GAMEPAD_MASK_L1, GP_SHAPE_ELLIPSE}}, \
             {GP_ELEMENT_BTN_BUTTON, {102, 23, 4, 4, 1, 1, GAMEPAD_MASK_B2, GP_SHAPE_ELLIPSE}}, \
-            {GP_ELEMENT_BTN_BUTTON, {82, 46, 4, 4, 1, 1, GAMEPAD_MASK_R2, GP_SHAPE_ELLIPSE}},  \
-            {GP_ELEMENT_BTN_BUTTON, {90, 40, 4, 4, 1, 1, GAMEPAD_MASK_L2, GP_SHAPE_ELLIPSE}},  \
-            {GP_ELEMENT_BTN_BUTTON, {93, 27, 4, 4, 1, 1, GAMEPAD_MASK_L3, GP_SHAPE_ELLIPSE}},  \
+            {GP_ELEMENT_BTN_BUTTON, {112, 24, 4, 4, 1, 1, GAMEPAD_MASK_R2, GP_SHAPE_ELLIPSE}}, \
+            {GP_ELEMENT_BTN_BUTTON, {122, 29, 4, 4, 1, 1, GAMEPAD_MASK_L2, GP_SHAPE_ELLIPSE}}, \
+            {GP_ELEMENT_BTN_BUTTON, {90, 52, 4, 4, 1, 1, GAMEPAD_MASK_L3, GP_SHAPE_ELLIPSE}},  \
+            {GP_ELEMENT_BTN_BUTTON, {90, 40, 4, 4, 1, 1, GAMEPAD_MASK_R3, GP_SHAPE_ELLIPSE}},  \
         {                                                                                      \
             GP_ELEMENT_BTN_BUTTON, { 82, 46, 4, 4, 1, 1, GAMEPAD_MASK_S1, GP_SHAPE_ELLIPSE }   \
         }                                                                                      \
